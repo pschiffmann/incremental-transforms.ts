859a993f49434699cc6fc6fc38004e4278b38dd5
Philipp Schiffmann <philippschiffmann93@gmail.com>
2021-07-21 13:31:19 +0200

    Support type predicate in IncrementalMap.filter()

--- src/map/filter.tsx
+++ src/map/filter.tsx
@@ -9,7 +9,13 @@ import {
   simplifyPatch,
 } from "./base";
 
-type FilteredIncrementalMapCallback<K, V, C extends Context> = (
+type IncrementalMapTypePredicate<K, IV, OV extends IV, C extends Context> = (
+  key: K,
+  value: IV,
+  context: UnpackContext<C>
+) => value is OV;
+
+type IncrementalMapFilter<K, V, C extends Context> = (
   key: K,
   value: V,
   context: UnpackContext<C>
@@ -19,11 +25,32 @@ type Dependencies<K, V, C extends Context> = Omit<C, "self"> & {
   readonly self: IncrementalMap<K, V>;
 };
 
+/**
+ * If the filter callback is a [type predicate][1], the value type of the
+ * resulting map will be of the type guarded by the type predicate. See this
+ * example for details:
+ *
+ * ```ts
+ * const a = $IncrementalMap.mutable<string, "a" | "b" | "c">();
+ * const b = $IncrementalMap.filter(a, function(k, v): v is "a" | "b" {
+ *   return v === "a" || "v" === "b";
+ * });
+ * // `b` will be of type IncrementalMap<string, "a" | "b">
+ * ```
+ *
+ * [1]: https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+ */
+export function filter<K, IV, OV extends IV, C extends Context>(
+  self: IncrementalMap<K, IV>,
+  callback: IncrementalMapTypePredicate<K, IV, OV, C>,
+  context?: C
+): IncrementalMap<K, OV>;
 export function filter<K, V, C extends Context>(
   self: IncrementalMap<K, V>,
-  callback: FilteredIncrementalMapCallback<K, V, C>,
+  callback: IncrementalMapFilter<K, V, C>,
   context?: C
-): IncrementalMap<K, V> {
+): IncrementalMap<K, V>;
+export function filter(self: any, callback: any, context?: any) {
   const result = new FilteredIncrementalMap(self, callback, context);
   result.connect();
   return result;
@@ -31,12 +58,13 @@ export function filter<K, V, C extends Context>(
 
 export class FilteredIncrementalMap<
   K,
-  V,
+  IV,
+  OV extends IV,
   C extends Context
-> extends IncrementalMapBase<K, V, Dependencies<K, V, C>> {
+> extends IncrementalMapBase<K, OV, Dependencies<K, IV, C>> {
   constructor(
-    self: IncrementalMap<K, V>,
-    callback: FilteredIncrementalMapCallback<K, V, C>,
+    self: IncrementalMap<K, IV>,
+    callback: IncrementalMapTypePredicate<K, IV, OV, C>,
     context?: C
   ) {
     super({ ...context, self } as any);
@@ -46,22 +74,22 @@ export class FilteredIncrementalMap<
     this.#callback = callback;
   }
 
-  #callback: FilteredIncrementalMapCallback<K, V, C>;
+  #callback: IncrementalMapTypePredicate<K, IV, OV, C>;
 
   _initialize(
     patches: Map<string, unknown>,
     hookRenderer: HookRenderer<K>
-  ): IncrementalMapPatch<K, V> | null {
-    const patch = createPatch<K, V>();
+  ): IncrementalMapPatch<K, OV> | null {
+    const patch = createPatch<K, OV>();
     const { self, ...context } = this.dependencies;
     const ctx = buildContext((context as unknown) as C, patches);
     const selfPatch = patches.get("self") as
-      | IncrementalMapPatch<K, V>
+      | IncrementalMapPatch<K, IV>
       | undefined;
 
     for (const [k, v] of getDirtyEntries(self, selfPatch, self.keys())) {
       const result = hookRenderer(k, () => this.#callback(k, v, ctx));
-      if (result) patch.updated.set(k, v);
+      if (result) patch.updated.set(k, v as OV);
     }
 
     return simplifyPatch(patch);
@@ -71,12 +99,12 @@ export class FilteredIncrementalMap<
     patches: Map<string, unknown>,
     dirtyKeys: Set<K>,
     hookRenderer: HookRenderer<K>
-  ): IncrementalMapPatch<K, V> | null {
-    const patch = createPatch<K, V>();
+  ): IncrementalMapPatch<K, OV> | null {
+    const patch = createPatch<K, OV>();
     const { self, ...context } = this.dependencies;
     const ctx = buildContext((context as unknown) as C, patches);
     const selfPatch = patches.get("self") as
-      | IncrementalMapPatch<K, V>
+      | IncrementalMapPatch<K, IV>
       | undefined;
 
     const contextChanged = patches.size !== Number(patches.has("self"));
@@ -87,7 +115,7 @@ export class FilteredIncrementalMap<
     )) {
       const result = hookRenderer(k, () => this.#callback(k, v, ctx));
       if (result !== this.has(k)) {
-        result ? patch.updated.set(k, v) : patch.deleted.add(k);
+        result ? patch.updated.set(k, v as OV) : patch.deleted.add(k);
       }
     }
     if (selfPatch) {
