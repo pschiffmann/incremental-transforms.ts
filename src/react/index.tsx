26a4b512fbdb7cbc66f8fc03df611c9b754f8957
Philipp Schiffmann <philippschiffmann93@gmail.com>
2022-04-19 16:15:54 +0200

    Add react hooks

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,11 +1,22 @@
-export function useIncrementalValue() {
-  throw new Error("Unimplemented");
+import { useSyncExternalStore } from "react";
+import { transaction } from "../core/index.js";
+import { IncrementalMap } from "../map/index.js";
+import { IncrementalValue } from "../value/index.js";
+
+function subscribe(onStoreChange: () => void): () => void {
+  transaction.on("commit", onStoreChange);
+  return function unsubscribe() {
+    transaction.off("commit", onStoreChange);
+  };
 }
 
-export function useIncrementalMap() {
-  throw new Error("Unimplemented");
+export function useIncrementalValue<T>(value: IncrementalValue<T>): T {
+  return useSyncExternalStore(subscribe, () => value.current);
 }
 
-export function useIncrementalMapKey() {
-  throw new Error("Unimplemented");
+export function useIncrementalMapGet<K, V>(
+  map: IncrementalMap<K, V>,
+  key: K
+): V | undefined {
+  return useSyncExternalStore(subscribe, () => map.get(key));
 }
